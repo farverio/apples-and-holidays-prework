dc9b989e113e3c4eb814a753372a53bdd363b6bb
Stephen F <stephen@farver.io>
2018-05-30 19:30:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
+    puts "#{season.split("_").capitalize}:"
     holiday.each do |name, list|
       output = "  "
       output.concat("#{name.capitalize}: ")
