1dba2d25eaafe97f7fb6ba8f29ce2d91a3538e30
Stephen F <stephen@farver.io>
2018-05-30 19:35:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,6 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season|
     season.each do |holiday, array_list|
+      puts array_list
       if array_list.include? "BBQ"
         matching_holidays << holiday
       end
