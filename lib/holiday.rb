2a96e7ad15ad9f5416f3ad1b61ce6ac9fcda7d83
Stephen F <stephen@farver.io>
2018-05-30 19:05:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,9 +55,9 @@ def all_winter_holiday_supplies(holiday_hash)
   combined_list = []
   
   holiday_hash[:winter].each do |holiday, supply_list|
-    puts "Before: #{combined_list}"
-    holiday[supply_list].each {|el| combined_list << el}
-    puts "After: #{combined_list}"
+    puts holiday
+    puts supply_list
+    # holiday[supply_list].each {|el| combined_list << el}
   end
   
   combined_list
